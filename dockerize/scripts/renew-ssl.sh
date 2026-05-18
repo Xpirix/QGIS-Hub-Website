66f6c8ea41291123636be9c49a352ebb0bfde815
Xpirix <xpirixii@gmail.com>
2026-05-18 12:30:05 +0300

    Refactor SSL configuration: remove acme-challenge.conf and inline settings in prod-ssl.conf

--- dockerize/scripts/renew-ssl.sh
+++ dockerize/scripts/renew-ssl.sh
@@ -11,7 +11,7 @@
 #    2021-11-12 11:10:00 UTC
 #    ...etc
 
-#25 11 * * * /bin/bash /home/web/QGIS-Hub-Website/dockerize/scripts/renew_ssl.sh > /tmp/ssl-renewal-logs.txt
+#25 11 * * * /bin/bash /home/QGIS-Hub-Website/dockerize/scripts/renew-ssl.sh > /tmp/ssl-renewal-logs.txt
 
 # Set common variables
 COMPOSE_FILE="/home/QGIS-Hub-Website/dockerize/docker-compose.yml"
